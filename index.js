a0995d300c873e2c04620ec69e6bb582b88e8eca
clement senou <clementsenou74@gmail.com>
2023-01-09 00:25:08 -0800

    commit tree d8fd54751b9a5045bcc0a61bfd9dee8194473560

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solutions in this file
+const writeCard = (names,event) => {
+  const messages = []
+  for (let i = 0; i< names.lenght; i++) {
+    const greetingMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    message.push(greetingMessage)
+  }
+  return messages
+}
+console.log(writeCard(["Charlie", "Sampip", "Ali"], "birthday"))
